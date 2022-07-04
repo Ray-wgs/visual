4776f37700f268f83c0eba486889035584c2730e
Ray-wgs <2388838628@qq.com>
2022-07-04 17:30:25 +0800

    fix config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 import VueSetupExtend from 'vite-plugin-vue-setup-extend'
-// element-plus 自动导入
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(),VueSetupExtend()],
@@ -10,18 +9,13 @@ export default defineConfig({
     // 配置路径别名
     alias: {
       '@': resolve(__dirname, './src'),
-      '@c': resolve(__dirname, './src/components'),
-      '@d': resolve(__dirname, './src/directives'),
-      '@p': resolve(__dirname, './src/plugins'),
-      '@a': resolve(__dirname, './src/assets'),
-      '@v': resolve(__dirname, './src/views'),
     },
   },
-//   css: {
-//     preprocessorOptions: {
-//       scss: {
-//         additionalData: `@use "@/styles/theme/theme-light.scss" as *;`,
-//       },
-//     },
-//   },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "@/styles/theme/index.scss" as *;`,
+      },
+    },
+  },
 })
