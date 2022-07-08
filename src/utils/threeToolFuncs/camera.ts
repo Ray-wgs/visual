13e3f8b0af851e2458d0cb7585459c033d796028
Grace <2388838628@qq.com>
2022-07-08 23:11:41 +0800

    improve camera func

--- src/utils/threeToolFuncs/camera.ts
+++ src/utils/threeToolFuncs/camera.ts
@@ -3,22 +3,21 @@ import { vsThreePoint ,vsThreeCameraOption,vsThreeCreateCameraOption} from "@/ty
 const camera = {
     /** 
     * 通过配置项生成相机
-    * @property {String} cType  要生成的相机类别，目前包含 OrthographicCamera,PerspectiveCamera
+    * @param {String} cType  要生成的相机类别，目前包含 OrthographicCamera,PerspectiveCamera
     * @param {vsThreePoint} cPosition 相机的初始位置
-    * @property cLookAt  相机的朝向的点
+    * @param cLookAt  相机的朝向的点
     * @param cOpts 相机的配置项，如可观测的位置的远近、视角的大小等
     * @return camera 生成的相机实例
     */ 
      creatCamera: ({cType,cPosition,cLookAt,cOpts}:vsThreeCreateCameraOption)=>{
         let camera:THREE.Camera
+        const {left,top,right,bottom,near,far,fov,aspect} = cOpts
         switch (cType) {
             case 'OrthographicCamera':
-                const {left,top,right,bottom,near:ognear,far:ogfar} = cOpts
-                camera = new THREE.OrthographicCamera(left,top,right,bottom,ognear,ogfar)
+                camera = new THREE.OrthographicCamera(left,top,right,bottom,near,far)
                 break;
             case 'PerspectiveCamera':
-                const {fov,aspect,near:ppnear,far:ppfar} = cOpts
-                camera = new THREE.PerspectiveCamera(fov,aspect,ppnear,ppfar)
+                camera = new THREE.PerspectiveCamera(fov,aspect,near,far)
                 break;
             default:
                 camera = new THREE.OrthographicCamera()
