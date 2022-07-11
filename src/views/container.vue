327623cf751a2b29039bf7d3f0dda6aebabc2452
Ray-wgs <2388838628@qq.com>
2022-07-11 15:01:14 +0800

    feat html2canvas

--- src/views/container.vue
+++ src/views/container.vue
@@ -20,16 +20,26 @@
 
         </vs-three>
     </div>
+    <el-button @click="onSave">
+
+    </el-button>
 </template>
 
 <script lang='ts' setup>
 import { reactive, toRefs,ref,onMounted} from 'vue'
 import {vsDragResizeStyle} from '@/types/dragResize.module'
 import {vsContainerData} from '@/types/container.module'
-const boardOption =ref<vsContainerData>({common:{threeBg:false,bg:''},comps:[
-    {tag:'vs-text',id:'1',propValue:{text:'12345678',running:true},style:{color:'#fff',width:'300px',height:'500px'}},
-    {tag:'vs-date-time',id:'2',propValue:{realTime:true},style:{minh:20,minw:120}}
-    ]})
+import html2canvas from 'html2canvas'
+const boardOption =ref<vsContainerData>({
+    common:{
+        threeBg:true,
+        bg:'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg.jj20.com%2Fup%2Fallimg%2Ftp09%2F210F2130512J47-0-lp.jpg&refer=http%3A%2F%2Fimg.jj20.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1660113230&t=862eb7ccd023bbc5024451f3e0d0daed'
+    },
+    comps:[
+        {tag:'vs-text',id:'1',propValue:{text:'12345678',running:true},style:{color:'#fff',width:'300px',height:'500px'}},
+        {tag:'vs-date-time',id:'2',propValue:{realTime:true},style:{minh:20,minw:120}}
+    ]
+})
 const boardContainer = ref()
 const active = ref<string|number>('null')
 const updateComp = (data:vsDragResizeStyle)=>{
@@ -40,6 +50,11 @@ const updateComp = (data:vsDragResizeStyle)=>{
         return comp.id == compId
     })
     if(comp) comp.style = {...comp.style,...data}
+}
+const onSave = async ()=>{
+     let canvas = await html2canvas(boardContainer.value)
+     console.log(canvas)
+     console.log(canvas.toDataURL("image/png"))
 }
 onMounted(()=>{
     if(!boardOption.value.common.threeBg){
