d2758bc05a404628ae2f5e726a6ca0e4f8c934c2
Ray-wgs <2388838628@qq.com>
2022-07-12 10:54:05 +0800

    improve vs-date-time component

--- src/components/DateTime.vue
+++ src/components/DateTime.vue
@@ -6,7 +6,7 @@
 
 <script lang='ts' setup name="vsDateTime">
 import dayjs from 'dayjs'
-import {toRefs,ref} from 'vue'
+import {toRefs,ref,watchEffect} from 'vue'
     const props = defineProps({
         // 时间格式化
         format:{
@@ -25,25 +25,30 @@ import {toRefs,ref} from 'vue'
         // 刷新周期
         refreshCycle:{
             type:Number,
-            default:1
+            default:1000
         }
     })
     const {format,defaultTime,realTime,refreshCycle} = toRefs(props)
+    let timer:number|undefined
     // 展示的时间
     const displayTime = ref('')
     // 更新展示的时间 默认1s更新一次
     const setTime = ()=>{
         displayTime.value = dayjs(new Date()).format(format.value) 
-        setTimeout(() => {
+        timer = window.setTimeout(() => {
             setTime()
-        }, refreshCycle.value*1000);
+        }, refreshCycle.value);
     }
-    // 开启实时时间后 开始自动刷新
-    if(realTime.value){
-        setTime()
-    }else{
-        displayTime.value = dayjs(defaultTime!.value || new Date()).format(format.value) 
-    }
+    
+    watchEffect(()=>{
+        // 开启实时时间后 开始自动刷新
+        if(realTime.value){
+            setTime()
+        }else{
+            if(timer) clearTimeout(timer)
+            displayTime.value = dayjs(defaultTime!.value || new Date()).format(format.value) 
+        }
+    })
 </script>
 <style scoped lang='scss'>
 .vs-date-time{
