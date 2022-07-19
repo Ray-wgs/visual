b7a9f634bb73cd05605ca084c4a935b5b3deff2c
Ray-wgs <2388838628@qq.com>
2022-07-19 09:59:50 +0800

    add vs-input-array component to chartOpt

---
+++ src/components/InputArray.vue
@@ -0,0 +1,22 @@
+<template>
+  <el-input v-model="val"  @change="onChangeArray"></el-input>
+</template>
+
+<script setup lang="ts" name="vsInputArray">
+import { reactive, toRefs,ref,} from 'vue'
+ const  props=defineProps({
+      array:{
+          require:true,
+          type:Array,
+          default:[]
+      },
+  })
+    const emit = defineEmits(['onChange'])
+    const data = reactive(props.array)
+    let val = ref('')
+    const onChangeArray= ()=>{
+      emit('onChange',val.value.split(','))
+    }
+</script>
+<style scoped lang='scss'>
+</style>
--- src/components/chartOpt/ChartItemOpt.vue
+++ src/components/chartOpt/ChartItemOpt.vue
@@ -1,17 +1,18 @@
 <template>
     <el-form-item :label="templateOpt.name" style="padding-left:20px">
         <el-input style="width: 200px" v-model="chartOpt.flattenOption[templateOpt.model]"  v-if="templateOpt.ui!.type === 'text'" :placeholder="templateOpt.ui!.placeholder"></el-input>
-            <el-input-number style="width: 200px" v-model="chartOpt.flattenOption[templateOpt.model]" :step="templateOpt.ui!.step" :min="templateOpt.ui!.min" :max="templateOpt.ui!.max" v-if="templateOpt.ui!.type === 'number'" ></el-input-number>
-            <el-switch v-model="chartOpt.flattenOption[templateOpt.model]" v-if="templateOpt.ui!.type === 'boolean'" ></el-switch>
-            <el-color-picker v-model="chartOpt.flattenOption[templateOpt.model]" show-alpha v-if="templateOpt.ui!.type === 'color'"></el-color-picker>
-            <el-select style="width: 200px" v-model="chartOpt.flattenOption[templateOpt.model]" placeholder="请选择" v-if="templateOpt.ui!.type === 'select'">
-                <el-option
-                    v-for="(item, index) in templateOpt.ui!.options"
-                    :key="index"
-                    :label="item.label"
-                    :value="item.value">
-                </el-option>
-            </el-select>
+        <el-input-number style="width: 200px" v-model="chartOpt.flattenOption[templateOpt.model]" :step="templateOpt.ui!.step" :min="templateOpt.ui!.min" :max="templateOpt.ui!.max" v-if="templateOpt.ui!.type === 'number'" ></el-input-number>
+        <el-switch v-model="chartOpt.flattenOption[templateOpt.model]" v-if="templateOpt.ui!.type === 'boolean'" ></el-switch>
+        <el-color-picker v-model="chartOpt.flattenOption[templateOpt.model]" show-alpha v-if="templateOpt.ui!.type === 'color'"></el-color-picker>
+        <el-select style="width: 200px" v-model="chartOpt.flattenOption[templateOpt.model]" placeholder="请选择" v-if="templateOpt.ui!.type === 'select'">
+            <el-option
+                v-for="(item, index) in templateOpt.ui!.options"
+                :key="index"
+                :label="item.label"
+                :value="item.value">
+            </el-option>
+        </el-select>
+        <vs-input-array  style="width: 200px" v-model="chartOpt.flattenOption[templateOpt.model]" v-if="templateOpt.ui!.type === 'array'" @onChange="(val:[])=> chartOpt.flattenOption[templateOpt!.model] = val"> </vs-input-array> 
     </el-form-item>
 </template>
 
@@ -20,6 +21,7 @@ import { reactive, toRefs,ref,PropType} from 'vue'
 import { useChartStore } from '@/stores/chart';
 import { storeToRefs } from 'pinia';
 import {vsChartTmeplate} from '@/types/chart.module'
+import vsInputArray from '@/components/InputArray.vue'
 const store = useChartStore()
 const {chartOpt} = storeToRefs(store)
 const props = defineProps({
